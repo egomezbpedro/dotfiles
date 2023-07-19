7cb1bcf854d1a7e7c192842459009e6b39eb4871
Pedro Enrique Gómez <57415533+egomezbpedro@users.noreply.github.com>
2023-07-19 11:04:41 +0200

    Remove exit statement in first if block

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ then
     curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
     echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
     sudo apt -qq update && sudo apt -y -qq install gum
-    exit
 fi
 
 gum format -- "# Welcome, let's configure this machine"
