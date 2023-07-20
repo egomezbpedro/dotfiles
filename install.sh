75d95124df98ddb0a28fb077a4623ac633cab193
Pedro Enrique Gómez <57415533+egomezbpedro@users.noreply.github.com>
2023-07-20 10:48:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,8 @@ echo "#################################"
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
 echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
-sudo apt -qq update && sudo apt -y -qq install gum
-
-gum format -- "# Welcome, let's configure this machine"
-
+sudo apt -qq update && sudo apt -y -qq install gum \
+gum format -- "# Welcome, let's configure this machine" \
 gum spin --spinner line --title "Installing gh cli" -- sleep 30 | type -p curl >/dev/null || (sudo apt -qq update && sudo apt -y -qq install curl) \
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
