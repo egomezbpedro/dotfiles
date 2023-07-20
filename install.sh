ba97adae2982c1440621c45bcecb4083763dc8da
Pedro Enrique Gómez <57415533+egomezbpedro@users.noreply.github.com>
2023-07-20 11:15:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,12 +11,13 @@ sudo apt -qq update && sudo apt -y -qq install gum
 
 gum format -- "# Welcome, let's configure this machine"
 
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+gum spin --spinner line --title "Feching Neovim prerequisites" -- sleep 30 | 
+type -p curl >/dev/null || (sudo apt -qq update && sudo apt -qq install curl -y) &&
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+&& sudo apt -qq update \
+&& sudo apt -qq install gh -y
 
 gum spin --spinner line --title "Feching Neovim prerequisites" -- sleep 30 | sudo apt-get -y -qq install ninja-build gettext cmake unzip
 gum spin --spinner line --title "Cloning Neovim" -- sleep 30 | git clone https://github.com/neovim/neovim ~/neovim
